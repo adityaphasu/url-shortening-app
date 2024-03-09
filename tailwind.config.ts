2347bf166fa1aa4b39ffc89d50d304de5d6e1d04
Aditya Phasu <adityaphasu919@gmail.com>
2024-03-09 23:29:15 +0530

    Add .prettierrc and update package.json and tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,17 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        cyan: "hsl(180, 66%, 49%)",
+        gray: "hsl(0, 0%, 75%)",
+        "dark-violet": "hsl(257, 27%, 26%)",
+        "secondary-red": "hsl(0, 87%, 67%)",
+        "grayish-violet": "hsl(257, 7%, 63%)",
+        "very-dark-blue": "hsl(255, 11%, 22%)",
+        "very-dark-violet": "hsl(260, 8%, 14%)",
+      },
+      fontFamily: {
+        poppins: "var(--font-poppins)",
       },
     },
   },
