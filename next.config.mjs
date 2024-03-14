9c54ce133cf88bb27075cc8119fddb02f680c8ef
Aditya Phasu <adityaphasu919@gmail.com>
2024-03-15 01:04:08 +0530

    Add next-pwa dependency and configure PWA settings

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  sw: "service-worker.js",
+});
+
+export default withPWA({});
