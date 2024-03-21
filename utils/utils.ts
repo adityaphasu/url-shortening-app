5503b7396da69997a3d080bcc22cb07d57b91b89
Aditya Phasu <adityaphasu919@gmail.com>
2024-03-21 18:19:18 +0530

    Update API endpoint for adding URLs

--- utils/utils.ts
+++ utils/utils.ts
@@ -1,7 +1,7 @@
 export const shortenUrl = async (url: string) => {
   try {
     const response = await fetch(
-      `https://csclub.uwaterloo.ca/~phthakka/1pt-express/addURL?long=${url}`,
+      `https://csclub.uwaterloo.ca/~phthakka/1pt-express/addURL.php?url=${url}`,
       {
         method: "POST",
       },
